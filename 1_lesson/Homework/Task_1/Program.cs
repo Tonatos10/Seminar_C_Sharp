e632d7830fae412d62e98b3e0a9d8fe6a1ecd51a
Tonatos10 <tonatos10@yandex.ru>
2022-11-25 21:49:28 +0300

    Homework_Task_1.0

--- 1_lesson/Homework/Task_1/Program.cs
+++ 1_lesson/Homework/Task_1/Program.cs
@@ -1,21 +1,25 @@
 ﻿// Напишите программу, которая на вход принимает два числа
 // и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Enter the first number ");
+Console.WriteLine("Введите первое число ");
 int num1 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter the second number ");
+Console.WriteLine("Введите второе число ");
 int num2 = int.Parse(Console.ReadLine());
 
 if (num1 > num2)
 {
-    Console.Write("The largest number ");
+    Console.Write("Большее число, ");
     Console.WriteLine($"{num1}");
+    Console.Write("Меньшее число, ");
+    Console.WriteLine($"{num2}");
 }
 else if (num1 < num2)
 {
-    Console.Write("The largest number ");
+    Console.Write("Большее число, ");
     Console.WriteLine($"{num2}");
+    Console.Write("Меньшее число, ");
+    Console.WriteLine($"{num1}");
 }
 else if (num1 == num2)
 {
---
+++ 1_lesson/Homework/Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Enter the first number ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the third number ");
+int num2 = int.Parse(Console.ReadLine());
