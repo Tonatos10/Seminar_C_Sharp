aae7087ad79632ae1b07217e249ff14374d0bb87
Tonatos10 <tonatos10@yandex.ru>
2022-11-25 18:27:40 +0300

    Task-2.0

--- 1_lesson/1.0/Program.cs
+++ 1_lesson/1.0/Program.cs
@@ -1,5 +1,8 @@
 ﻿// 0. Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя).
+
 Console.WriteLine("Write a number: "); // Вывод сообщения
+
 int num = int.Parse(Console.ReadLine()); // Указываем, что num это целое число (int num) 
 //и преобразовываем строку в число int.Parse(Console.ReadLine())
+
 Console.WriteLine(num * num); 
---
+++ 1_lesson/1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+
+Console.WriteLine("Enter the number 1 "); // Вывод сообщения
+int num2 = int.Parse(Console.ReadLine()); // Присваивание значения
+
+Console.WriteLine("Enter the number 2 "); // Вывод сообщения
+int num1 = int.Parse(Console.ReadLine()); // Присваивание значения
+
+if(num2 == num1 * num1)
+{
+    Console.WriteLine("Yes"); // Если условие выполняется, то Yes
+}
+
+else
+    Console.WriteLine("No"); // Если условие не выполняется, то No
