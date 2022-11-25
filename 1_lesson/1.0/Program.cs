9e66662bc2d33b54d9f242866bd57de7881c32d5
Tonatos10 <tonatos10@yandex.ru>
2022-11-25 14:45:31 +0300

    Task-1.0

--- 1_lesson/1.0/Program.cs
+++ 1_lesson/1.0/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя).
+Console.WriteLine("Write a number: "); // Вывод сообщения
+int num = int.Parse(Console.ReadLine()); // Указываем, что num это целое число (int num) 
+//и преобразовываем строку в число int.Parse(Console.ReadLine())
+Console.WriteLine(num * num); 
