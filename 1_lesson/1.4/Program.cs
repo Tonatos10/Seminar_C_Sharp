1f7d30d272c35ee11083103ee36502eaac9d95a1
Tonatos10 <tonatos10@yandex.ru>
2022-11-25 19:56:31 +0300

    Task-1.4

---
+++ 1_lesson/1.4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу вычисления модуля числа
+
+Console.Write("Enter the number ");
+
+var s = Console.ReadLine();
+
+int N = int.Parse(s);
+
+Console.WriteLine($"{Math.Abs(N)}"); // Добавили уже готовую математическую функцию
