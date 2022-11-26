ae0b3f45f578f6a91d26284506be29761739847d
Tonatos10 <tonatos10@yandex.ru>
2022-11-27 00:02:03 +0300

    2_Lecture_Task_1.3

---
+++ 2_Lecture/Task_1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int[] array = new int[10]; // Определяем новый массив и указываем,
+// что в этом массиве будет 10 элементов. По умолчанию данный массив
+// будет наполнен нулями (0).
+
+FillArray(array); // Метод FillArray - заполнил нам массив.
+PrintArray(array); // Метод PrintArray - распечатал значения.
