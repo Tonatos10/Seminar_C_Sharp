feaf85cc3207ea0f449577381e2f63b65d979120
Tonatos10 <tonatos10@yandex.ru>
2022-11-29 22:34:43 +0300

    2_lesson_Task_2.2

---
+++ 2_lesson/2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+
+string Multiple(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0) // && значит, что левое и правое выражения истины.
+        return "кратно";
+    else
+        return "не кратно";
+}
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(Multiple(number));
