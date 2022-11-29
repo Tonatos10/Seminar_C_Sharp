411500bc958e4eaba4e985f15563f2e20410b7c6
Tonatos10 <tonatos10@yandex.ru>
2022-11-30 01:44:33 +0300

    2_lesson_HW_Task_1

---
+++ 2_lesson/Homework/HW_Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+string TwoSecond(int num)
+
+{
+    if (num > 99 && num < 1000)
+    {
+        int result = ((num / 10) % 10);
+        return $"{result} ";
+    }
+    else
+        return "Введите трехзначное число";
+}
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(TwoSecond(number));
