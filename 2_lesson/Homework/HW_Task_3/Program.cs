e138ed528aad6a5588df53f8fc5991b7dd62f5c6
Tonatos10 <tonatos10@yandex.ru>
2022-11-30 11:17:59 +0300

    2_lesson_HW_Task_3

--- 2_lesson/Homework/HW_Task_3/Program.cs
+++ 2_lesson/Homework/HW_Task_3/Program.cs
@@ -1,2 +1,19 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного
 // числа или сообщает, что третьей цифры нет.
+
+string ThreeSecond(int num)
+{
+    if (num > 99)
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        return $"{num % 10} ";
+    }
+    else
+        return "Третьей цифры нет";
+}
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(ThreeSecond(num));
