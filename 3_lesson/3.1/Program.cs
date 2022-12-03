2e2a544e0b70e0df33519de078fa0bd0f6fe8b7e
Tonatos10 <tonatos10@yandex.ru>
2022-12-03 23:49:05 +0300

    3_lesson Task_3.1

---
+++ 3_lesson/3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает дмапазон возможных координат точек в этой четверти (x и Y).
+
+void Quarter(string num)
+{
+    if (num == "I")
+        Console.WriteLine("x > 0 && y > 0");
+    else if (num == "II")
+        Console.WriteLine("x < 0 && y > 0");
+    else if (num == "III")
+        Console.WriteLine("x < 0 && y < 0");
+    else if (num == "IV")
+        Console.WriteLine("x > 0 && y < 0");
+    else
+        Console.WriteLine("error");
+}
+Console.Write("Введите номер четверти: ");
+Quarter(Console.ReadLine());
