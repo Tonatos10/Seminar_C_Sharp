e7dab23e0ec1260a7d8d031554215b5cffbb293b
Tonatos10 <tonatos10@yandex.ru>
2022-12-04 19:45:06 +0300

    3_lesson_HW_3.1

---
+++ 3_lesson/Homework/3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 3D пространстве.
+
+double Distance(int ax, int ay, int az, int bx, int by, int bz)
+{
+    double result;
+    result = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+    return result;
+}
+
+Console.WriteLine("Введите aX:");
+int ax = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите aY:");
+int ay = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите aZ:");
+int az = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите bX:");
+int bx = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите bY:");
+int by = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите bZ:");
+int bz = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Distance(ax, ay, az, bx, by, bz), 2));
