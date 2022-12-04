bf5f16a57abb20a7980f0163ac49cf50dbfb6746
Tonatos10 <tonatos10@yandex.ru>
2022-12-04 19:39:13 +0300

    3_lesson_HW_3.0

---
+++ 3_lesson/Homework/3.0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+
+string Polindrom(int N)
+{
+    int N2 = N;
+    int num = 0;
+    int reverse = 0;
+    while (N > 0)
+    {
+        num = N % 10;
+        reverse = reverse * 10 + num;
+        N = N / 10;
+    }
+    if (N2 == reverse)
+        return "Да";
+    else
+        return "Нет";
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int chislo = int.Parse(Console.ReadLine());
+Console.WriteLine(Polindrom(chislo));
