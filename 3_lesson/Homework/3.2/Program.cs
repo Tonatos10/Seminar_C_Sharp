70939713092fced0be602a76d3036763fd9076a1
Tonatos10 <tonatos10@yandex.ru>
2022-12-04 19:52:52 +0300

    3_lesson_HW_3.2

---
+++ 3_lesson/Homework/3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Kub(int N)
+{
+    int i = 0;
+    double result;
+    while (i < N)
+    {
+        i += 1;
+        if (i == (N))
+        {
+            result = Math.Pow(i, 3);
+            Console.Write($"{result}");
+        }
+        else
+        {
+
+            result = Math.Pow(i, 3);
+            Console.Write($"{result},");
+        }
+    }
+}
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Kub(num);
