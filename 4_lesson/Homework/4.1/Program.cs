1b4bf662af2892b7a3f9a13f395a022e4a6a2bab
Tonatos10 <tonatos10@yandex.ru>
2022-12-08 00:23:17 +0300

    4_lesson HW

---
+++ 4_lesson/Homework/4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int NatDegree(int A, int B)
+{
+    int n = 1;
+    
+    for (int i = 1; i <= B; i++)
+    {
+        n = n * A;
+    }
+    return n;
+}
+
+Console.WriteLine("Введите число1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(NatDegree(num1, num2));
---
+++ 4_lesson/Homework/4.3/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// случайными числами и выводит их на экран.
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 100);
+Console.WriteLine($"[{string.Join(", ", array)}]");
