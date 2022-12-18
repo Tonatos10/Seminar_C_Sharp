ba30a5dc4c4f6315e320b6d63cdd46c5a23cd1ac
Tonatos10 <tonatos10@yandex.ru>
2022-12-19 02:25:15 +0300

    8_lesson Task 8.0 - 8.3

---
+++ 8_lesson/8.3/Program.cs
@@ -0,0 +1,38 @@
+﻿// 4. Задайте двумерный массив из целых чисел. Напишите программу,
+//    которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+
+int[] MinEl(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int min_num = arr[0, 0];
+    int[] indexes = new int[2];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            if (min_num > arr[i, j])
+            {
+                min_num = arr[i, j];
+                indexes[0] = i;
+                indexes[1] = j;
+            }
+    }
+    Console.WriteLine(arr[indexes[0], indexes[1]]);
+    return indexes;
+}
+
+void WithoutRowColumn(int[,] arr, int[] m_indexes)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            if (m_indexes[0] == i || m_indexes[1] == j) continue;
+            else Console.Write($"{arr[i, j],3}");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
